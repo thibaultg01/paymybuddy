578b138ee609fb80ce554709cec3edb0876227c6
thibaultg01 <thi.bault.guittoun@gmail.com>
2025-07-15 15:52:33 +0200

    Move exception EmailAlreadyExistsException in GlobalExceptionHandler + delte WebExceptionHandler.java file

--- src/main/java/com/paymybuddy/exception/GlobalExceptionHandler.java
+++ src/main/java/com/paymybuddy/exception/GlobalExceptionHandler.java
@@ -2,11 +2,16 @@ package com.paymybuddy.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.paymybuddy.model.User;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -15,11 +20,14 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    /*@ExceptionHandler(UserNotFoundException.class)
-    public String handleUserNotFound(UserNotFoundException ex, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("error", ex.getMessage());
-        return "redirect:/transfer";
-    }*/
+	@ExceptionHandler(EmailAlreadyExistsException.class)
+    public String handleEmailExists(EmailAlreadyExistsException ex,
+                                    @ModelAttribute("user") User user,
+                                    BindingResult result,
+                                    Model model) {
+        result.rejectValue("email", "error.user", ex.getMessage());
+        return "register";
+    }
 	
 	@ExceptionHandler(UserNotFoundException.class)
 	public String handleUserNotFound(UserNotFoundException ex, RedirectAttributes redirectAttributes) {
